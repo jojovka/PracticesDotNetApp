c3da2898b5923eff4de21bcfab630620d27a84ff
Kazhi Berik <bkazhi@gmail.com>
2020-04-11 12:58:45 +0600

    First commit

---
+++ Try4/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Try4
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, Microsoft.Extensions.Hosting.IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "calculator",
+                    template: "Calculator/{action}/{number:int}",
+                    defaults: new { Controller = "Calculator" });
+
+                routes.MapRoute(
+                    name: "messages",
+                    template: "say/{**message}",
+                    defaults: new { controller = "Messages", action = "ShowMessage" });
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Hello}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
